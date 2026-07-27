384d452b4dd03ff8c05a04f1e116b665145da081
rizikMurkaUltra <murka55556@gmail.com>
2026-07-27 19:17:00 +0300

    Сделан back (кнопка которая возвращает)
    делаю кнопку use (пока не готова)

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 
+// переменные
+let back_btn = document.querySelector('.back-btn')
+let use_btn = document.querySelectorAll('.use_btn')
+
+// возврат на страничку
+
+if (back_btn) {
+    back_btn.addEventListener('click', function() {
+        window.history.back()
+    })
+}
+
+// добавление товара в корзину
+
+if(use_btn.length > 0) {
+    use_btn.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const card = event.target.closest('.cart-menu');
+            const title = card.querySelector('.p-menu1').textContent;
+            const priceText = card.querySelector('.p-menu2').textContent;
+            const imgSrc = card.querySelector('.first-part img').getAttribute('src');
+            const price = parseInt(priceText.replace('$', ''));
+        })
+    })
+}
