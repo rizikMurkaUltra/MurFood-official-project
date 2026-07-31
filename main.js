146d48d9c78b1102d89e2e3b07e2ca4371c61afb
MrMurka <murka55556@gmail.com>
2026-07-31 17:36:46 +0300

    Проверка все ли встало нормально
    .

--- main.js
+++ main.js
@@ -21,6 +21,63 @@ if(use_btn.length > 0) {
             const priceText = card.querySelector('.p-menu2').textContent;
             const imgSrc = card.querySelector('.first-part img').getAttribute('src');
             const price = parseInt(priceText.replace('$', ''));
-        })
+            const product = {
+                title: title,
+                price: price,
+                img: imgSrc,
+                count: 1,
+            };
+            let cart = JSON.parse(localStorage.getItem('cart')) || [];
+            if(existingProduct) {
+                existingProduct.count + 1;
+            } else {
+                cart.push(product);
+            }
+
+            localStorage.setItem('cart', JSON.stringify(cart));
+            window.location.href = 'shop.html';
+        });
     })
 }
+
+
+
+// страница корзина вывод товаров и подсчет
+
+const foodGrid = document.querySelector('.food-grid');
+const buyAllBtn = document.querySelector('.buy-all-btn')
+
+if(foodGrid) {
+    function  renderCart() {
+        let cart =  JSON.parse(localStorage.getItem('cart')) || []
+        foodGrid.innerHTML = '';
+        if(cart.length === 0) {
+            foodGrid.innerHTML = '<p class ="empty-card-text">Your cart is empty</p>';
+            if(buyAllBtn) buyAllBtn.textContent = 'buy all(0$)'
+            return
+        }
+
+        let totalPrice = 0;
+        
+
+        cart.forEach((product, index) => {
+            const itemPrice = product.price * product.count;
+            totalPrice += itemPrice;
+
+            const cardHTML = `
+                <div class = "cart-menu" data-index="${index}
+                    <p class ="p-1menu">${product.title} (x${product.count}</p>
+                    <div>
+                        <img src="${product.img}" alt="${product.title}">
+                    </div>
+                    <button class="delete-btn">delete</button>
+                    <p class="p2-menu">${item.price}$</p>
+                </div>`;
+            foodGrid.insertAdjacentHTML('beforeend', cardHTML)
+        });
+
+    }
+}
+
+
+
